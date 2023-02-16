283bbd74611644867a9abc2947caf43745c582f8
maahmp1 <marcelamartinspena@gmail.com>
2023-02-16 12:25:37 -0300

    preparing the application

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,7 +1,6 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
@@ -11,7 +10,6 @@ import { TabsPage } from './tabs.page';
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
     TabsPageRoutingModule
   ],
   declarations: [TabsPage]
