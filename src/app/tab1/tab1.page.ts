943bcad7ed2d2ba8969e625a45b398a587d8cc50
maahmp1 <marcelamartinspena@gmail.com>
2023-03-16 12:25:35 -0300

    att: tab 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
   cara = 'src/assets/cara.png';
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+
 export class Tab2Page {
+
+  pontos: number = 1;
+  time1: number = 0;
+  time2: number = 0;
 
   constructor() {}
 
+  adicionarPontos(valor: number) {
+    this.pontos = valor;
+  }
+
+  somapontos(valor: number) {
+    valor += this.pontos;
+  }
+
+  zerarPontos() {
+    this.pontos = 0;
+    this.time1 = 0;
+    this.time2 = 0;
+  }
 }
